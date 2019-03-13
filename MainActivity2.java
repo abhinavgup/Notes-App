8fe9a59e56b4a832a8410d475131fdb6fdf741b8
abhinavgup <31439689+abhinavgup@users.noreply.github.com>
2019-03-13 07:15:47 +0530

    Create MainActivity2.java

---
+++ MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.hp.appnotes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+public class notesactivity extends AppCompatActivity {
+    int noteid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notesactivity);
+
+        EditText editText =(EditText)findViewById(R.id.editText);
+
+        Intent intent= getIntent();
+
+        noteid= intent.getIntExtra("noteid",-1);
+
+        if(noteid!=-1){
+
+            editText.setText(MainActivity.note.get(noteid));
+        }
